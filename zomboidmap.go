a6dcc47164755a39d1f53afbfe80e2193c4672cd
Nathan Lutterman <nathan@blodgett-lutterman.com>
2022-07-20 23:44:05 -0700

    Remove unused stuff

--- zomboidmap.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "gitlab.com/nlutterman/zmodinigen/steamworkshop"
-
-type ZomboidMap struct {
-	*steamworkshop.Item
-	MapFolders []string
-}
--- zomboidmod.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "gitlab.com/nlutterman/zmodinigen/steamworkshop"
-
-type ZomboidMod struct {
-	*steamworkshop.Item
-	ModIDs []string
-}
